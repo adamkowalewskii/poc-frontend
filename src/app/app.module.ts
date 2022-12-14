f6e3e9219bd58b79122993fb959667835e897a15
BartoszKuziemski <bartuspl99@gmail.com>
2022-12-14 10:47:37 +0100

    login import fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from "./component/login/login.component";
 
 @NgModule({
   declarations: [
@@ -17,4 +17,5 @@ import { LoginComponent } from './login/login.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
